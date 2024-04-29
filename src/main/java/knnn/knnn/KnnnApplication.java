88210d288379d63dfd277f33f0bf3fb844e1b02f
nima <linux.vps.vpn@gmail.com>
2024-04-29 22:12:57 +0330

    first

---
+++ src/main/java/knnn/knnn/KnnnApplication.java
@@ -0,0 +1,13 @@
+package knnn.knnn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnnnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnnnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/knnn/knnn/KnnnApplicationTests.java
@@ -0,0 +1,13 @@
+package knnn.knnn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnnnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
